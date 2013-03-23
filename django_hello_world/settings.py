99fab0d9ab35d95f7631ce6d10436ad45f4ca6b8
breaf <mail@breaf.net>
2013-03-23 17:04:53 +0200

    Added TEMPLATE_CONTEXT_PROCESSORS to settings

--- django_hello_world/settings.py
+++ django_hello_world/settings.py
@@ -92,6 +92,13 @@ TEMPLATE_LOADERS = (
 #     'django.template.loaders.eggs.Loader',
 )
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.request',
+)
+
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
