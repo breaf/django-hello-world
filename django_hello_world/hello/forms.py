d33890ad862ae00c031052dce811c351648bffcc
breaf <mail@breaf.net>
2013-03-25 13:41:46 +0200

    Added form to template

--- django_hello_world/hello/forms.py
+++ django_hello_world/hello/forms.py
@@ -1,8 +1,17 @@
 from django import forms
+from django.contrib.auth.models import User
 from hello.models import UserProfile
+
+
+class UserForm(forms.ModelForm):
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email')
 
 
 class ProfileForm(forms.ModelForm):
 
     class Meta:
         model = UserProfile
+        fields = ('date_of_birth', 'jabber', 'skype', 'bio', 'contacts', 'photo')
--- django_hello_world/hello/views.py
+++ django_hello_world/hello/views.py
@@ -1,13 +1,27 @@
 #-*- coding: utf-8 -*-
 from annoying.decorators import render_to
 from django.contrib.auth.models import User
+from hello.forms import ProfileForm, UserForm
 from hello.models import RequestRecord
 
 
 @render_to('hello/home.html')
 def home(request):
+    if request.method == 'POST':
+        user_form = UserForm(request.POST, instance=request.user)
+        profile_form = ProfileForm(request.POST, request.FILES, instance=request.user.get_profile())
+        if user_form.is_valid():
+            user_form.save()
+        else:
+            print user_form.errors
+        if profile_form.is_valid():
+            profile_form.save()
+        else:
+            print profile_form.errors
     users = User.objects.filter()
-    return {'users': users}
+    user_form = UserForm(instance=request.user)
+    profile_form = ProfileForm(instance=request.user.get_profile())
+    return {'users': users, 'user_form': user_form, 'profile_form': profile_form}
 
 
 @render_to('hello/requests.html')
