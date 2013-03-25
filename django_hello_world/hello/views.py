c44eaee1d598b57d3f3969e8830a3ff21d37ca69
breaf <mail@breaf.net>
2013-03-25 13:50:39 +0200

    Fixed home view

--- django_hello_world/hello/views.py
+++ django_hello_world/hello/views.py
@@ -7,21 +7,25 @@ from hello.models import RequestRecord
 
 @render_to('hello/home.html')
 def home(request):
-    if request.method == 'POST':
-        user_form = UserForm(request.POST, instance=request.user)
-        profile_form = ProfileForm(request.POST, request.FILES, instance=request.user.get_profile())
-        if user_form.is_valid():
-            user_form.save()
-        else:
-            print user_form.errors
-        if profile_form.is_valid():
-            profile_form.save()
-        else:
-            print profile_form.errors
     users = User.objects.filter()
-    user_form = UserForm(instance=request.user)
-    profile_form = ProfileForm(instance=request.user.get_profile())
-    return {'users': users, 'user_form': user_form, 'profile_form': profile_form}
+    if request.user.is_authenticated():
+        if request.method == 'POST':
+            user_form = UserForm(request.POST, instance=request.user)
+            profile_form = ProfileForm(request.POST, request.FILES, instance=request.user.get_profile())
+            if user_form.is_valid():
+                user_form.save()
+            else:
+                print user_form.errors
+            if profile_form.is_valid():
+                profile_form.save()
+            else:
+                print profile_form.errors
+        else:
+            user_form = UserForm(instance=request.user)
+            profile_form = ProfileForm(instance=request.user.get_profile())
+        return {'users': users, 'user_form': user_form, 'profile_form': profile_form}
+    else:
+        return {'users': users}
 
 
 @render_to('hello/requests.html')
