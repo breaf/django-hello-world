4abb6669f27fba49bdbde5f648a2f743755cb35f
breaf <mail@breaf.net>
2013-03-26 19:11:44 +0200

    ticket:#3 Updated tests

--- django_hello_world/hello/tests.py
+++ django_hello_world/hello/tests.py
@@ -8,19 +8,29 @@ Replace this with more appropriate tests for your application.
 from django.core.urlresolvers import reverse
 from django.test import TestCase
 from django.test.client import Client
-
-
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+from hello.models import RequestRecord
 
 
 class HttpTest(TestCase):
+
     def test_home(self):
         c = Client()
         response = c.get(reverse('home'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, '42 Coffee Cups Test Assignment')
+
+    def test_records(self):
+        before_count = RequestRecord.objects.count()
+        c = Client()
+        response = c.get(reverse('home'))
+        after_count = RequestRecord.objects.count()
+        self.assertGreater(after_count, before_count)
+
+    def test_requests(self):
+        c = Client()
+        for i in range(10):
+            response = c.get(reverse('home'))
+        response = c.get(reverse('requests_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.context['records']), 10)
+        self.assertEqual(response.context['records'][0].id, 1)
