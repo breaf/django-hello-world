1c4a5846f32d642b52a864288cea673926308dd7
breaf <mail@breaf.net>
2013-03-26 13:21:31 +0200

    ticket:#6 Added calendar widget

--- django_hello_world/settings.py
+++ django_hello_world/settings.py
@@ -76,9 +76,7 @@ ADMIN_MEDIA_PREFIX = '/media/admin/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    path('static'),
 )
 
 # List of finder classes that know how to find static files in
