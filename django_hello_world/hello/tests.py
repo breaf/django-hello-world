54a676270efa75579f4fc0bf506bd1a23b9496bb
breaf <mail@breaf.net>
2013-03-26 20:07:37 +0200

    ticket:#4 Changed tests

--- django_hello_world/hello/tests.py
+++ django_hello_world/hello/tests.py
@@ -5,23 +5,15 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 
-from django.core.urlresolvers import reverse
+from django.http import HttpRequest
+from django.template import Template, RequestContext
 from django.test import TestCase
-from django.test.client import Client
 from django.conf import settings
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
-
-
 class HttpTest(TestCase):
+
     def test_home(self):
-        c = Client()
-        response = c.get(reverse('home'))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, settings.DEBUG)
+        context = RequestContext(HttpRequest())
+        result = Template('{{ settings.DEBUG }}').render(context)
+        self.assertEqual(result, str(settings.DEBUG))
