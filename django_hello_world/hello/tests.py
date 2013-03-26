a79f03a0001bf2c78865338e8ea1e6e2cdd7c8ff
breaf <mail@breaf.net>
2013-03-27 01:09:43 +0200

    ticket:#5 Updated tests

--- django_hello_world/hello/tests.py
+++ django_hello_world/hello/tests.py
@@ -7,12 +7,28 @@ Replace this with more appropriate tests for your application.
 
 from django.core.urlresolvers import reverse
 from django.test import TestCase
-from django.test.client import Client
+from django.contrib.auth.models import User
+from settings import path
 
 
 class HttpTest(TestCase):
-    def test_home(self):
-        c = Client()
-        response = c.get(reverse('home'))
+
+    def test_home_login(self):
+        self.assertTrue(self.client.login(username='admin', password='admin'))
+        response = self.client.get(reverse('home'))
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, '42 Coffee Cups Test Assignment')
+        self.assertContains(response, 'form')
+
+    def test_post(self):
+        self.assertTrue(self.client.login(username='admin', password='admin'))
+        response = self.client.post(reverse('home'), {'first_name': 'test_name'})
+        self.assertEqual(response.status_code, 200)
+        user = User.objects.get(username='admin')
+        self.assertEqual(user.first_name, 'test_name')
+
+    def test_photo_upload(self):
+        self.client.login(username='admin', password='admin')
+        p = path('hello', 'static', 'avatar.jpg')
+        with open(p, 'rb') as photo:
+            response = self.client.post(reverse('home'), {'photo': photo})
+        self.assertContains(response, 'id="avatar"')
