128686b8fc2df6238dd2ec6ff2f1574b16cefefa
breaf <mail@breaf.net>
2013-03-28 12:48:21 +0200

    ticket:9 Finished command

--- django_hello_world/hello/management/commands/printmodels.py
+++ django_hello_world/hello/management/commands/printmodels.py
@@ -1,17 +1,16 @@
 # -*- coding: utf-8 -*-
 from django.core.management.base import BaseCommand, CommandError
-from hello.models import UserProfile, RequestRecord
+from django.db.models import get_app, get_models
 
 
 class Command(BaseCommand):
     help = 'This command prints all project models and the count of objects in every model.'
 
     def handle(self, *args, **options):
-        profile_model = UserProfile
-        request_model = RequestRecord
-
-        result = ""
-        self.stdout.write('Models: %s \n' % result)
-        self.stderr.write('error: \n')
-        # raise CommandError(result)
+        models_dict = {}
+        app = get_app('hello')
+        for model in get_models(app):
+            models_dict[model.__name__] = model.objects.count()
+        self.stdout.write('%s \n' % str(models_dict))
+        self.stderr.write('error: %s \n' % str(models_dict))
 
